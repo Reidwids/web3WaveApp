e010112f7b4863aec1d2d0c3cfb57192b2b56cca
Reidwids <reidwids@gmail.com>
2022-08-07 13:11:33 -0400

    Local contract deployment

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.4;
+// SPDX-License-Identifier: UNLICENSED
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
